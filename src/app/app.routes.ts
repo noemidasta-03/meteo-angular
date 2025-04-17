14e3f511db3865c81e153a213b357568ba57011f
Noemi D'Asta <noemi.dasta@dt.local>
2025-04-17 17:14:04 +0200

    add dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 import {
   WeatherContainerComponentComponent
 } from './component/weather-container-component/weather-container-component.component';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  {path:'',component:WeatherContainerComponentComponent}
+  {path:'', component:DashboardComponent },
+  {path:'weather', component:WeatherContainerComponentComponent},
+
 ];
