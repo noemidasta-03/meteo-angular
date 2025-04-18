887671f3c8b31b7ba62bdfb70973bfdd469bf71b
Noemi D'Asta <noemi.dasta@dt.local>
2025-04-18 10:28:23 +0200

    finish project meteo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,9 @@ import { Routes } from '@angular/router';
 import {
   WeatherContainerComponentComponent
 } from './component/weather-container-component/weather-container-component.component';
-import {DashboardComponent} from './component/dashboard/dashboard.component';
+
 
 export const routes: Routes = [
-  {path:'', component:DashboardComponent },
-  {path:'weather', component:WeatherContainerComponentComponent},
+  {path:'', component:WeatherContainerComponentComponent},
 
 ];
