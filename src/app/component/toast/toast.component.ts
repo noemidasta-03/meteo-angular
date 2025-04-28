53f39573ce8ec0806c36dd06904698e94c050b40
Noemi D'Asta <noemi.dasta@dt.local>
2025-04-28 11:21:17 +0200

    update angular

--- src/app/component/toast/toast.component.ts
+++ src/app/component/toast/toast.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-toast',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css'
 })
 export class ToastComponent {
+  message:string='';
+  isVisible:boolean = false;
 
+  showToast(message:string){
+    this.message = message;
+    this.isVisible = true;
+
+    setTimeout(()=>{
+      this.isVisible = false;
+    },3000)
+  }
 }
